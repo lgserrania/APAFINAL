d3d0e1d18c83696de3540ed71162a48dbb5b94f2
lgserrania <32219493+lgserrania@users.noreply.github.com>
2019-01-08 13:21:51 -0200

    Add files via upload

--- Util.java
+++ Util.java
@@ -5,6 +5,8 @@
  */
 package apafinal;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Gustavo
@@ -17,6 +19,30 @@ public class Util {
     public static double distancia(Cliente c1, Cliente c2){
         double result = Math.sqrt(Math.pow((c2.x - c1.x), 2) + Math.pow((c2.y - c1.y), 2));
         return result;
+    }
+    
+    public static boolean validaRota(ArrayList<Cliente> clientes){
+        double carga = 0;
+        double tempo = Util.distancia(base, clientes.get(0));
+        
+        if(tempo < clientes.get(0).inicio){
+            tempo += clientes.get(0).inicio - tempo;
+        }
+        tempo += clientes.get(0).atendimento;
+        carga += clientes.get(0).demanda;
+        
+        for(int i = 1; i < clientes.size() - 1; i++){
+            tempo += Util.distancia(clientes.get(i - 1), clientes.get(i));
+            if(tempo > clientes.get(i).fim) return false;
+            if(tempo < clientes.get(i).inicio) tempo += clientes.get(i).inicio - tempo;
+            tempo += clientes.get(i).atendimento;
+            carga += clientes.get(i).demanda;
+            if(carga > capacidade) return false;
+        }
+        
+        tempo += Util.distancia(clientes.get(clientes.size() - 1), base);
+        if(tempo > clientes.get(clientes.size() - 1).fim) return false;
+        return true;
     }
     
 }
